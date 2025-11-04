9d337a287ceec8e0b5d3e7206255ac2c0be0919a
4meta5 <asinghchrony@protonmail.com>
2025-11-04 17:04:40 -0500

    working relay template

---
+++ runtimes/relay/build.rs
@@ -0,0 +1,14 @@
+#[cfg(all(not(feature = "metadata-hash"), feature = "std"))]
+fn main() {
+    substrate_wasm_builder::WasmBuilder::build_using_defaults();
+}
+
+#[cfg(all(feature = "metadata-hash", feature = "std"))]
+fn main() {
+    substrate_wasm_builder::WasmBuilder::init_with_defaults()
+        .enable_metadata_hash("UNIT", 12)
+        .build();
+}
+
+#[cfg(not(feature = "std"))]
+fn main() {}
